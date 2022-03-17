bb48b3242b14945931831df1388ab0946b36224b
KumJungMin <rmawjdals@naver.com>
2022-03-17 19:39:13 +0900

    2. 테스트 코드를 우선 작성해보자

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-function refinetText(s) {
-  return s.replace("    ", "  ").replace("  ", " ");
+// 테스트가 통과되도록 운영코드를 고쳐보자!
+// 함수 refinetText 목적
+// - 모든 띄어쓰기(space, tab)를 하나의 공백으로 변경하는 것
+// - 인자로 넘어온 특정 문자를 마스킹하는 것
+function refinetText(s, options) {
+  s = s
+    .replace("     ", " ")
+    .replace("\t", "")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("mockist", "*******")
+    .replace("purist", "******");
+  if (options) {
+    for (const bannedWord of options.bannedWords) {
+      s = s.replace(bannedWord, "*".repeat(bannedWord.length));
+    }
+  }
+  return s;
 }
 module.exports = refinetText;
--- index.test.js
+++ index.test.js
@@ -1,42 +1,61 @@
+const { faker } = require("@faker-js/faker");
 const sut = require("./index"); // sut === systemUnderTest
 
-// 1. 테스트 나열
-test('sut transform "hello  world" to "hello world"', () => {
-  const result = sut("hello  world"); //success
-  expect(result).toBe("hello world");
-});
-
-test('sut transform "hello    world" to "hello world"', () => {
-  const result = sut("hello    world"); // success
-  expect(result).toBe("hello world");
-});
-
-test('sut transform "hello   world" to "hello world"', () => {
-  const result = sut("hello   world"); // fail
-  expect(result).toBe("hello world");
-});
-
-// 2. 반복문으로 테스트 간략화
-// -> 이 경우 실패한 경우에 대한 input값은 알 수 없음;
-// -> 우선 실행된 테스트가 실패하면, 그 이후 경우에 대한 테스트가 불가;
-// -> 코드를 줄인다고 좋은 게 아님 -> 코드 품질 저하ㅜ
-test('sut correctly works"', () => {
-  for (const source of ["hello  world", "hello   world", "hello    world"]) {
-    const result = sut(source);
-    expect(result).toBe("hello world");
-  }
-});
-
-// 3. ParameterizedTest 사용
-// -> 하나의 테스트 메소드로 여러 개의 파라미터에 대해서 테스트 가능
-// -> 코드 중복은 줄이되 테스트 품질 보장 가능
-// 에러시: sut transforms "hello    world" to "hello world" 로 나옴
+// ParameterizedTest
+// : 하나의 테스트 메소드로 여러 개의 파라미터에 대해서 테스트 가능
 test.each`
-  source              | expected
-  ${"hello  world"}   | ${"hello world"}
-  ${"hello   world"}  | ${"hello world"}
-  ${"hello    world"} | ${"hello world"}
-`('sut transforms "$source" to "$expected"', ({ source, expect }) => {
+  source                  | expected
+  ${"hello  world"}       | ${"hello world"}
+  ${"hello   world"}      | ${"hello world"}
+  ${"hello    world"}     | ${"hello world"}
+  ${"hello     world"}    | ${"hello world"}
+  ${"hello      world"}   | ${"hello world"}
+  ${"hello       world"}  | ${"hello world"}
+  ${"hello        world"} | ${"hello world"}
+`('sut transforms "$source" to "$expected"', ({ source, expected }) => {
   const result = sut(source);
   expect(result).toBe(expected);
+});
+
+test.each`
+  source             | expected
+  ${"hello\t world"} | ${"hello world"}
+  ${"hello \tworld"} | ${"hello world"}
+`(
+  'sut transforms "$source" that contains tab character to "$expected"',
+  ({ source, expected }) => {
+    const result = sut(source);
+    expect(result).toBe(expected);
+  }
+);
+
+// ---------------------------------------------------------------
+// 테스트 우선 개발을 해보자
+// 테스트 코드를 우선 작성하고 -> 운영 코드를 작성
+// 이때 테스트할 문자를 하나씩 추가하는 게 아니라, 랜덤문자로 테스트하도록 해야함
+
+// bad case
+test.each`
+  source             | bannedWords              | expected
+  ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+  ${"hello purist"}  | ${["mockist", "purist"]} | ${"hello ******"}
+`(
+  "sut transforms $source to $expected",
+  ({ source, bannedWords, expected }) => {
+    const result = sut(source, { bannedWords });
+    expect(result).toBe(expected);
+  }
+);
+
+// good case
+// facker를 사용해 임의의 문자를 생성해서 테스트하기
+describe("given banned word", () => {
+  const bannedWord = faker.lorem.word();
+  const source = `hello ${bannedWord}`;
+  const expected = `hello ${"*".repeat(bannedWord.length)}`;
+
+  test(`${bannedWord} then invoke sut then it returns ${expected}`, () => {
+    const result = sut(source, { bannedWords: [bannedWord] });
+    expect(result).toBe(expected);
+  });
 });
